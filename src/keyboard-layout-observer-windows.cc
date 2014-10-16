16515505ea66862d73da4062e0c1362be3e0bb5a
Kevin Sawicki <kevinsawicki@gmail.com>
2014-10-16 13:30:37 -0700

    Implement getCurrentKeyboardLayout on Windows

---
+++ src/keyboard-layout-observer-windows.cc
@@ -0,0 +1,52 @@
+#include "keyboard-layout-observer.h"
+#include <windows.h>
+
+using namespace v8;
+
+void KeyboardLayoutObserver::Init(Handle<Object> target) {
+  NanScope();
+  Local<FunctionTemplate> newTemplate = NanNew<FunctionTemplate>(KeyboardLayoutObserver::New);
+  newTemplate->SetClassName(NanNew<String>("KeyboardLayoutObserver"));
+  newTemplate->InstanceTemplate()->SetInternalFieldCount(1);
+  Local<ObjectTemplate> proto = newTemplate->PrototypeTemplate();
+  NODE_SET_METHOD(proto, "getCurrentKeyboardLayout", KeyboardLayoutObserver::GetCurrentKeyboardLayout);
+  target->Set(NanNew<String>("KeyboardLayoutObserver"), newTemplate->GetFunction());
+}
+
+NODE_MODULE(keyboard_layout_observer, KeyboardLayoutObserver::Init)
+
+NAN_METHOD(KeyboardLayoutObserver::New) {
+  NanScope();
+
+  Local<Function> callbackHandle = args[0].As<Function>();
+  NanCallback *callback = new NanCallback(callbackHandle);
+
+  KeyboardLayoutObserver *observer = new KeyboardLayoutObserver(callback);
+  observer->Wrap(args.This());
+  NanReturnUndefined();
+}
+
+uv_loop_t *loop = uv_default_loop();
+uv_async_t async;
+
+KeyboardLayoutObserver::KeyboardLayoutObserver(NanCallback *callback) : callback(callback) {
+}
+
+KeyboardLayoutObserver::~KeyboardLayoutObserver() {
+  delete callback;
+};
+
+void KeyboardLayoutObserver::HandleKeyboardLayoutChanged() {
+}
+
+NAN_METHOD(KeyboardLayoutObserver::GetCurrentKeyboardLayout) {
+  NanScope();
+
+  char layoutName[KL_NAMELENGTH];
+  GetKeyboardLayoutName(layoutName);
+
+  if (::GetKeyboardLayoutNameW(mCurrentLocaleName))
+    NanReturnValue(NanUndefined(NanNew<String>(layoutName)));
+  else
+    NanReturnValue(NanUndefined());
+}
