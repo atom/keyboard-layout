b60f2da5ce544f0ab28faa4187f7c7e67db50e42
Paul Betts <paul@paulbetts.org>
2015-02-17 11:48:34 -0800

    Copy over some string methods

--- src/keyboard-layout-observer-windows.cc
+++ src/keyboard-layout-observer-windows.cc
@@ -1,7 +1,49 @@
+#undef _WIN32_WINNT
+#define _WIN32_WINNT 0x0601
+
+#undef WINVER
+#define WINVER 0x0601
+
 #include "keyboard-layout-observer.h"
+
+#include <string>
 #include <windows.h>
 
 using namespace v8;
+
+std::string ToUTF8(const std::wstring& string) {
+  if (string.length() < 1) {
+    return std::string();
+  }
+
+  // NB: In the pathological case, each character could expand up
+  // to 4 bytes in UTF8.
+  int cbLen = (string.length()+1) * sizeof(char) * 4;
+  char* buf = new char[cbLen];
+  int retLen = WideCharToMultiByte(CP_UTF8, 0, string.c_str(), string.length(), buf, cbLen, NULL, NULL);
+  buf[retLen] = 0;
+
+  std::string ret;
+  ret.assign(buf);
+  return ret;
+}
+
+std::wstring ToWString(const std::string& string) {
+  if (string.length() < 1) {
+    return std::wstring();
+  }
+
+  // NB: If you got really unlucky, every character could be a two-wchar_t
+  // surrogate pair
+  int cchLen = (string.length()+1) * 2;
+  wchar_t* buf = new wchar_t[cchLen];
+  int retLen = MultiByteToWideChar(CP_UTF8, 0, string.c_str(), strlen(string.c_str()), buf, cchLen);
+  buf[retLen] = 0;
+
+  std::wstring ret;
+  ret.assign(buf);
+  return ret;
+}
 
 void KeyboardLayoutObserver::Init(Handle<Object> target) {
   NanScope();
